68428c0c1272a9e2e617dd1a4ab3170bd9173d26
Tellesy <mtellesy@gmail.com>
2024-11-16 15:54:06 +0200

    project init

---
+++ src/main/java/ly/kaizen/lad/LadApplication.java
@@ -0,0 +1,13 @@
+package ly.kaizen.lad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ly/kaizen/lad/LadApplicationTests.java
@@ -0,0 +1,13 @@
+package ly.kaizen.lad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
